a5cfd37c654e0759411ee4ac2955d447f69fac74
TesvonU <164524113+TesvonU@users.noreply.github.com>
2024-04-15 11:18:01 +0200

    Update core.py

--- core.py
+++ core.py
@@ -4,6 +4,7 @@ import warnings
 
 # settings for easier console display
 warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
 pd.set_option('display.width', 320)
 pd.set_option('display.max_columns', None)
 
@@ -13,18 +14,35 @@ def read_file(path):
     columns, lines = read_column_line(dataset)
     return (dataset, columns, lines)
 
+
 def read_column_line(dataset):
     columns = list(dataset.columns)
     lines = len(dataset)
-    return (columns, lines)
+    return columns, lines
+
 
 def print_dataset(dataset):
     print(dataset)
 
+
+def drop_column(dataset, to_drop):
+    dataset = dataset.drop(columns=[to_drop])
+    columns, lines = read_column_line(dataset)
+    return (dataset, columns, lines)
+
+
 output = read_file("champions.csv")
 dataset = output[0]
-columns = output[1]
-lines = output[2]
+column = output[1]
+line = output[2]
 print_dataset(dataset)
-print_dataset(columns)
-print_dataset(lines)
+print_dataset(column)
+print_dataset(line)
+
+output = drop_column(dataset, column[-1])
+dataset = output[0]
+column = output[1]
+line = output[2]
+print_dataset(dataset)
+print_dataset(column)
+print_dataset(line)
